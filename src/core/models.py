e489dce7ba09aee49765829018fa4d74fc6816f4
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-01-14 20:52:08 -0300

    feat: modelo

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+
+    # def __str__(self) -> str:
+    #     return f"{self.apellidos}, {self.nombres}"
