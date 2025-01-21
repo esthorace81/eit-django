6e727d436ecfb7b52880043dd23ae064cbed5785
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-01-21 23:48:43 +0000

    feat: listview para modelo Categoria

--- src/producto/views.py
+++ src/producto/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
     return render(request, 'producto/index.html')
+
+
+def categoria_list(request):
+    categorias = models.Categoria.objects.all()
+    context = {'categorias': categorias}
+    return render(request, 'producto/categoria_list.html', context)
