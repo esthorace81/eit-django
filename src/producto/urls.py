bc7d0975696770a626669fc477aa91e10fb8bb4a
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-01-28 22:42:49 +0000

    fix: url

--- src/producto/urls.py
+++ src/producto/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
 from . import views
+from .views_models import categoria
 
 app_name = 'producto'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('categoria/list/', views.categoria_list, name='categoria_list'),
-    path('categoria/create/', views.categoria_create, name='categoria_create'),
-    path('categoria/update/<int:pk>', views.categoria_update, name='categoria_update'),
-    path('categoria/detail/<int:pk>', views.categoria_detail, name='categoria_detail'),
-    path('categoria/delete/<int:pk>', views.categoria_delete, name='categoria_delete'),
+    path('categoria/list/', categoria.categoria_list, name='categoria_list'),
+    path('categoria/create/', categoria.categoria_create, name='categoria_create'),
+    path('categoria/update/<int:pk>', categoria.categoria_update, name='categoria_update'),
+    path('categoria/detail/<int:pk>', categoria.categoria_detail, name='categoria_detail'),
+    path('categoria/delete/<int:pk>', categoria.categoria_delete, name='categoria_delete'),
 ]
